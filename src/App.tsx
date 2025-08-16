d721832f38b73e6a0936e2147aeeed53645ba62a
SubudhiK <ksatya.subudhi@unisys.com>
2025-08-17 03:46:06 +0530

    few more enhancements

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
 import NightSky from "./components/NightSky";
+import Galaxy from "./components/Galaxy";
+import FlipCard from "./components/FlipCard";
+import Avatar from "./components/Avatar";
 
 const App: React.FC = () => {
   return (
@@ -16,6 +19,9 @@ const App: React.FC = () => {
       <Element name="skills"><Skills /></Element>
       <Element name="contact"><Contact /></Element>
       <Element name="contact"><NightSky /></Element>
+      <Element name="contact"><Galaxy /></Element>
+      <Element name="contact"><FlipCard front="abc" back="abc" /></Element>
+      <Element name="contact"><Avatar /></Element>
     </div>
   );
 };
